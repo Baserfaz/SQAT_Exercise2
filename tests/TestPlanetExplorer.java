d5a36bd311c5daafa8cddf759fe53d7a6142f001
somename <someemail>
2016-10-21 16:22:28 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -13,73 +13,73 @@ public class TestPlanetExplorer {
 	@Test
 	public void executeCommand_start() throws PlanetExplorerException {
 		PlanetExplorer pe = new PlanetExplorer(3, 3, "");
-		assertEquals("(0, 0, N)", pe.executeCommand(""));
+		assertEquals("(0,0,N)", pe.executeCommand(""));
 	}
 	
 	@Test
 	public void executeCommand_f() throws PlanetExplorerException {
 		PlanetExplorer pe = new PlanetExplorer(3, 3, "");
-		assertEquals("(0, 1, N)", pe.executeCommand("f"));
+		assertEquals("(0,1,N)", pe.executeCommand("f"));
 	}
 	
 	@Test
 	public void executeCommand_r() throws PlanetExplorerException {
 		PlanetExplorer pe = new PlanetExplorer(3, 3, "");
-		assertEquals("(0, 0, E)", pe.executeCommand("r"));
+		assertEquals("(0,0,E)", pe.executeCommand("r"));
 	}
 	
 	@Test (expected = PlanetExplorerException.class)
 	public void executeCommand_input_error() throws PlanetExplorerException {
 		PlanetExplorer pe = new PlanetExplorer(3, 3, "");
-		pe.executeCommand("o,123");
+		pe.executeCommand("o123");
 	}
 	
 	@Test 
 	public void executeCommand_rrrr() throws PlanetExplorerException {
 		PlanetExplorer pe = new PlanetExplorer(3, 3, "");
-		assertEquals("(0, 0, N)", pe.executeCommand("rrrr"));
+		assertEquals("(0,0,N)", pe.executeCommand("rrrr"));
 	}
 	
 	@Test
 	public void executeCommand_rlrl() throws PlanetExplorerException {
 		PlanetExplorer pe = new PlanetExplorer(3, 3, "");
-		assertEquals("(0, 0, N)", pe.executeCommand("rlrl"));
+		assertEquals("(0,0,N)", pe.executeCommand("rlrl"));
 	}
 	
 	@Test
 	public void executeCommand_ffrf() throws PlanetExplorerException {
 		PlanetExplorer pe = new PlanetExplorer(3, 3, "");
-		assertEquals("(1, 2, E)", pe.executeCommand("ffrf"));
+		assertEquals("(1,2,E)", pe.executeCommand("ffrf"));
 	}
 	
 	@Test
 	public void executeCommand_ffrfff() throws PlanetExplorerException {
 		PlanetExplorer pe = new PlanetExplorer(3, 3, "");
-		assertEquals("(0, 2, E)", pe.executeCommand("ffrfff"));
+		assertEquals("(0,2,E)", pe.executeCommand("ffrfff"));
 	}
 	
 	@Test
 	public void executeCommand_wrap_left_to_right() throws PlanetExplorerException {
 		PlanetExplorer pe = new PlanetExplorer(3, 3, "");
-		assertEquals("(2, 0, W)", pe.executeCommand("lf"));
+		assertEquals("(2,0,W)", pe.executeCommand("lf"));
 	}
 	
 	@Test
 	public void executeCommand_wrap_right_to_left() throws PlanetExplorerException {
 		PlanetExplorer pe = new PlanetExplorer(3, 3, "");
-		assertEquals("(0, 0, E)", pe.executeCommand("rfff"));
+		assertEquals("(0,0,E)", pe.executeCommand("rfff"));
 	}
 	
 	@Test
 	public void executeCommand_wrap_top_to_bottom() throws PlanetExplorerException {
 		PlanetExplorer pe = new PlanetExplorer(3, 3, "");
-		assertEquals("(0, 0, N)", pe.executeCommand("fff"));
+		assertEquals("(0,0,N)", pe.executeCommand("fff"));
 	}
 	
 	@Test
 	public void executeCommand_wrap_bottom_to_top() throws PlanetExplorerException {
 		PlanetExplorer pe = new PlanetExplorer(3, 3, "");
-		assertEquals("(0, 2, N)", pe.executeCommand("b"));
+		assertEquals("(0,2,N)", pe.executeCommand("b"));
 	}
 	
 	@Test
