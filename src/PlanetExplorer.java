92d1efef6a002108528b97de41391f3ff3688258
somename <someemail>
2016-10-21 15:46:18 +0300

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -13,56 +13,46 @@ public class PlanetExplorer {
 	
 	private Facing myFacing = Facing.N;
 	
-	// myPosition[0] => X
-	// myPosition[1] => Y
+	// myPosition[0] => X-axis
+	// myPosition[1] => Y-axis
 	private int[] myPosition = { 0 , 0 };
 	
-	// map
+	// map of all obstacles in a map
+	// true => it's an obstacle.
 	private boolean[][] obstacleMap;
 	
 	// these are needed for the borders
 	private int mapWidth = 0;
 	private int mapHeight = 0;
 	
-	
 	public PlanetExplorer(int x, int y, String obstacles){
-	/*	x and y represent the size of the grid.
-	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" with no white spaces. 
-	 *  
-		Example use: For a 100x100 grid with two obstacles at coordinates (5,5) and (7,8)
-		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)")  
-		 
-	 */
 		
 		mapHeight = y;
 		mapWidth = x;
 		obstacleMap = new boolean[x][y];
 		
+		// create a character array of obstacle's positional information.
+		char[] oArr = obstacles.toLowerCase().toCharArray();
+		
 		// populate obstacleMap with obstacles
-		char[] obstacleCharArr = new char[];
+		for(int i = 0; i < oArr.length; i++) {
+			
+			if(oArr[i])
+			
+			
+		}
 		
 	}
 	
 	public boolean IsObstacle(int x, int y) {
-		boolean b = false;
 		
 		
 		
 		
-		return b;
+		return false;
 	}
 	
 	public String executeCommand(String command) throws PlanetExplorerException{
-		
-		/* The command string is composed of "f" (forward), "b" (backward), "l" (left) and "r" (right)
-		 * Example: 
-		 * The explorer is on a 100x100 grid at location (0, 0) and facing NORTH. 
-		 * The explorer is given the commands "ffrff" and should end up at (2, 2) facing East.
-		 
-		 * The return string is in the format: "(pos_x,pos_y,facing)(obs1_x,obs1_y)(obs2_x,obs2_y)..(obsN_x,obsN_y)" 
-		 * Where pos_x and pos_y are the final coordinates, facing is the current direction the explorer is pointing to (N,S,W,E).
-		 * The return string should also contain a list of coordinates of the encountered obstacles. No white spaces.
-		 */
 		
 		// create a character array of the commands.
 		char[] commands = command.toLowerCase().toCharArray();
